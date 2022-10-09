c6dd0090595e0c21ce3ca3742df79c54d445ab25
ziluzhu-1936068 <ziluzhu@uw.edu>
2022-10-09 13:35:42 -0700

    firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import { Home } from './component/Home';
 import { Detail } from './component/Event';
 import { WholeList } from './component/WholeList';
 import { bsList } from './component/record';
+// import { initializeApp } from 'firebase/app';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCS-8l0whqAOODU7US5ex1hZ4FfoE7WDDA",
+//   authDomain: "info270a1.firebaseapp.com",
+//   projectId: "info270a1",
+//   storageBucket: "info270a1.appspot.com",
+//   messagingSenderId: "114903139883",
+//   appId: "1:114903139883:web:2de94ffb7ea24283684c6e"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCS-8l0whqAOODU7US5ex1hZ4FfoE7WDDA",
+  authDomain: "info270a1.firebaseapp.com",
+  projectId: "info270a1",
+  storageBucket: "info270a1.appspot.com",
+  messagingSenderId: "114903139883",
+  appId: "1:114903139883:web:2de94ffb7ea24283684c6e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
